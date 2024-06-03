7cfae0985d98561b0454c818ceddb9a564429eb8
Julian Rubisch <julian@julianrubisch.at>
2024-06-03 13:38:34 +0200

    feat: Eager load includes in `find_record`

--- lib/avo/base_resource.rb
+++ lib/avo/base_resource.rb
@@ -213,9 +213,21 @@ module Avo
       end
 
       def find_record(id, query: nil, params: nil)
+        query = query || find_scope # If no record is given we'll use the default
+
+        if includes.present?
+          query = query.includes(*includes)
+        end
+
+        if attachments.present?
+          attachments.each do |attachment|
+            query = query.send("with_attached_#{attachment}")
+          end
+        end
+
         Avo::ExecutionContext.new(
           target: find_record_method,
-          query: query || find_scope, # If no record is given we'll use the default
+          query: query,
           id: id,
           params: params
         ).handle
